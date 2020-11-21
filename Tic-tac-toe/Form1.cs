1a7eea25675af4dba13c5210d9526c0e9945fef0
Atharv Gupta <raw.atharv12@gmail.com>
2020-11-21 00:17:32 +0000

    game finished

--- Tic-tac-toe/Form1.cs
+++ Tic-tac-toe/Form1.cs
@@ -12,9 +12,101 @@ namespace Tic_tac_toe
 {
     public partial class Form1 : Form
     {
+
+        bool turn = true;
+        int turnCount = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Atharv Gupta", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turnCount++;
+            checkWinner();
+        }
+
+        private void checkWinner()
+        {
+            foreach(Control c in Controls)
+            {
+                bool prescenceOfWinner = false;
+
+                if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled)) prescenceOfWinner = true;
+                else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled)) prescenceOfWinner = true;
+                else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled)) prescenceOfWinner = true;
+
+                else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled)) prescenceOfWinner = true;
+                else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled)) prescenceOfWinner = true;
+                else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled)) prescenceOfWinner = true;
+
+                else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled)) prescenceOfWinner = true;
+                else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled)) prescenceOfWinner = true;
+
+                if (prescenceOfWinner)
+                {
+                    disableButtons();
+
+                    String winner = "";
+                    if (turn)
+                        winner = "O";
+                    else
+                        winner = "X";
+
+                    MessageBox.Show(winner + " wins!", "Result!");
+                }
+                else
+                {
+                    if (turnCount == 9) MessageBox.Show("Draw", "Result!");
+                }
+            }
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turnCount = 0;
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch { }
         }
     }
 }
